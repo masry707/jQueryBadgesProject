f4d469c88f6610134a4728d97a5d8a344d4805b1
Ahmed El-Masry <ahmed.elmasry707@gmail.com>
2017-07-22 15:43:39 +0200

    Added Ajax request to get & show all completed courses of CodeSchool
    Signed-off-by: Ahmed El-Masry <ahmed.elmasry707@gmail.com>

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,23 @@
 $(function() {
 
   // your code will go here
+  url = 'https://www.codeschool.com/users/smartec.json'
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function(response) {
+      // console.log(response)
+      completed_courses = response.courses.completed;
+      for(var i = 0; i < completed_courses.length; i++) {
+        course_div = ['<div class="course">'];
+        course_div.push('<h3>' + completed_courses[i].title + '</h3>');
+        course_div.push('<img src="' + completed_courses[i].badge + '" alt="' +
+        completed_courses[i].title + '">');
+        course_div.push('<a href="' + completed_courses[i].url + '" class="btn btn-primary" target="_blank">See Course</a>');
+        course_div.push('</div>');
+        $('#badges').append(course_div.join(' '));
+      }
+    }
+  });
 
 });
